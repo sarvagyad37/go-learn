0bc76b6c43a284834c84db1f9e32fce3f5caf42a
Sarvagya Dwivedi <67410071+sarvagyad37@users.noreply.github.com>
2023-10-18 15:34:53 -0400

    Create main.go

---
+++ go-movies-crud/main.go
@@ -0,0 +1,14 @@
+package main
+
+// Movie Struct (Model)
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
